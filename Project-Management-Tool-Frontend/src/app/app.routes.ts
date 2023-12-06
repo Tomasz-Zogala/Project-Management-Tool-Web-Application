6ca890dd3e6935fc5b05f52af2e38bea2ad41405
Tomasz Żogała <tomasz.jakub.zogala@gmail.com>
2023-12-06 10:29:12 +0100

    Styles

--- Project-Management-Tool-Frontend/src/app/app.routes.ts
+++ Project-Management-Tool-Frontend/src/app/app.routes.ts
@@ -4,8 +4,6 @@ import {SignInComponent} from "./sign-in/sign-in.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {ProjectsComponent} from "./projects/projects.component";
 import {CompanyComponent} from "./company/company.component";
-import {ProfileComponent} from "./profile/profile.component";
-import {SettingsComponent} from "./settings/settings.component";
 import {TasksComponent} from "./tasks/tasks.component";
 import {PageErrorComponent} from "./page-error/page-error.component";
 import {UsersComponent} from "./users/users.component";
@@ -15,9 +13,7 @@ export const routes: Routes = [
   { path: 'tasks', component: TasksComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'company', component: CompanyComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'companies', component: CompanyComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'signin', component: SignInComponent },
   { path: '**', component: PageErrorComponent },
--- Project-Management-Tool-Frontend/src/app/services/auth.service.ts
+++ Project-Management-Tool-Frontend/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {UserService} from "./user.service";
 import {catchError, map, of} from "rxjs";
+import {Router} from "@angular/router";
 
 
 @Injectable({
@@ -10,13 +11,14 @@ import {catchError, map, of} from "rxjs";
 export class AuthService {
   public isLoggedIn : boolean = false;
 
-  constructor(private userService : UserService) {
+  constructor(private userService : UserService, private router : Router) {
   }
   signIn(email: string, password: string) {
     this.userService.getUserByEmail(email).pipe(
       map((user) => {
         if (user) {
           if (password === user.password) {
+            this.router.navigate(['/']);
             this.isLoggedIn = true;
           } else {
             console.error('Incorrect password');
