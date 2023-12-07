2a85812d35c3a6913418619a112ed91f775046de
Tomasz Żogała <tomasz.jakub.zogala@gmail.com>
2023-12-07 11:17:45 +0100

    My Profile page

--- Project-Management-Tool-Frontend/src/app/profile/profile.component.ts
+++ Project-Management-Tool-Frontend/src/app/profile/profile.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+
+constructor(public authService : AuthService) {
+}
 
 }
